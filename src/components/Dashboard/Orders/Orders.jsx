cc2cd64f51932b989211314e01f625516b685917
Maryam Fadaee <maryamfadaee@gmail.com>
2024-08-09 14:05:10 -0400

    add table and style it

--- src/components/Dashboard/Orders/Orders.jsx
+++ src/components/Dashboard/Orders/Orders.jsx
@@ -1,4 +1,17 @@
-import { Box, Flex, Text, Divider } from "@chakra-ui/react";
+import {
+	Box,
+	Flex,
+	Text,
+	Divider,
+	Table,
+	Thead,
+	Tbody,
+	Tr,
+	Th,
+	Td,
+	TableCaption,
+	TableContainer,
+} from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import { getAllOrders } from "../../../services/TradeServices";
 const Orders = () => {
@@ -49,17 +62,90 @@ const Orders = () => {
 			borderWidth={"2px"}
 			borderRadius={"10px"}
 		>
-			<Flex m={5} p={5} mb={0} pb={0} justifyContent={"space-between"}>
+			<Flex>
 				<Box width={"50%"}>
-					<Text fontWeight={500} fontSize={"18px"}>
+					<Text
+						m={0}
+						width={"100%"}
+						height={"100%"}
+						pl={"3"}
+						pt={2}
+						pb={1}
+						fontWeight={"bold"}
+						fontSize={"18px"}
+					>
 						{" "}
 						Recent Orders
 					</Text>
 				</Box>
 			</Flex>
-			<Divider />
-
-			<Box p={4} height={"400px"} overflowY={"auto"}>
+			<Divider p={0} m={0} />
+			<Box
+				pl={4}
+				bg="white"
+				w="100%"
+				h="480px"
+				overflowY="auto"
+				borderRadius={"10px"}
+				boxShadow={
+					"rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px"
+				}
+				border={"2x"}
+				borderColor={"#F1D7D7"}
+			>
+				<Table
+					mt={5}
+					sx={{ "tbody tr:nth-of-type(odd)": { bg: "accentColor" } }}
+				>
+					<Thead position="sticky" top={0} zIndex={1}>
+						<Tr>
+							<Th p={0} textAlign={"center"} pb={2}>
+								Symbol
+							</Th>
+							<Th p={0} textAlign={"center"} pb={2}>
+								Type
+							</Th>
+							<Th p={0} textAlign={"center"} pb={2}>
+								Quantity
+							</Th>
+							<Th p={0} textAlign={"center"} pb={2}>
+								Price
+							</Th>
+							<Th p={0} textAlign={"center"} pb={2}>
+								Status
+							</Th>
+							<Th p={0} textAlign={"center"} pb={2}>
+								Date
+							</Th>
+						</Tr>
+					</Thead>
+					<Tbody>
+						{orders.map((o, index) => (
+							<Tr key={index}>
+								<Td p={1} textAlign={"center"} color={"blue"}>
+									{o.stockSymbol}
+								</Td>
+								<Td p={1} textAlign={"center"}>
+									{o.orderType}/{o.tradeType}
+								</Td>
+								<Td p={1} textAlign={"center"}>
+									{o.quantity}
+								</Td>
+								<Td p={1} textAlign={"center"}>
+									${o.price.toFixed(2)}
+								</Td>
+								<Td p={1} textAlign={"center"}>
+									{o.status}
+								</Td>
+								<Td p={1} textAlign={"center"}>
+									{formatDate(o.createdAt)}
+								</Td>
+							</Tr>
+						))}
+					</Tbody>
+				</Table>
+			</Box>
+			{/* <Box p={4} height={"400px"} overflowY={"auto"}>
 				<Flex
 					mb={4}
 					fontWeight="bold"
@@ -106,7 +192,7 @@ const Orders = () => {
 						</Flex>
 					))}
 				</Box>
-			</Box>
+			</Box> */}
 		</Box>
 	);
 };
