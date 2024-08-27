086b7187685a0fa2ea7578b4251fd00e90c23d84
Maryam Fadaee <maryamfadaee@gmail.com>
2024-08-27 11:56:29 -0400

    set alignment of the navbar items

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,110 +23,86 @@ function NavbarMenu() {
 			justifyContent={"space-between"}
 			alignItems={"center"}
 			pr={"5%"}
+			pl={"2%"}
+			color={"white"}
 		>
-			<Flex
-				justifyContent="space-between"
-				alignItems="center"
-				width="100%"
-				color={"white"}
-			>
-				<Flex>
-					<Link to="/home">
-						<Box
-							bg={
-								isActive("/home")
-									? "rgba(255, 255, 255, 0.1)"
-									: ""
-							}
-							p={{ base: 1, md: 2 }}
-							m={{ base: 0, md: 2 }}
-							borderRadius={"5px"}
-							_hover={{
-								backgroundColor: "rgba(255, 255, 255, 0.1)",
-								borderRadius: "5px",
-							}}
-						>
-							Home
-						</Box>
-					</Link>
-					<Link to="/about">
-						<Box
-							bg={
-								isActive("/about")
-									? "rgba(255, 255, 255, 0.1)"
-									: ""
-							}
-							_hover={{
-								backgroundColor: "rgba(255, 255, 255, 0.1)",
-								borderRadius: "5px",
-							}}
-							borderRadius={"5px"}
-							p={{ base: 1, md: 2 }}
-							m={{ base: 0, md: 2 }}
-						>
-							About
-						</Box>
-					</Link>
-					<Link to="/services">
-						<Box
-							bg={
-								isActive("/services")
-									? "rgba(255, 255, 255, 0.1)"
-									: ""
-							}
-							borderRadius={"5px"}
-							_hover={{
-								backgroundColor: "rgba(255, 255, 255, 0.1)",
-								borderRadius: "5px",
-							}}
-							p={{ base: 1, md: 2 }}
-							m={{ base: 0, md: 2 }}
-						>
-							Services
-						</Box>
-					</Link>
-					<Link to={"/dashboard"}>
-						<Flex
-							bg={
-								isActive("/dashboard")
-									? "rgba(255, 255, 255, 0.1)"
-									: ""
-							}
-							borderRadius={"5px"}
-							borderWidth={"2px"}
-							width={"100%"}
-							alignItems={"center"}
-							justifyContent={"center"}
-							borderColor={"white"}
-							_hover={{
-								backgroundColor: "rgba(255, 255, 255, 0.1)",
-								borderRadius: "5px",
-							}}
-							p={{ base: 1, md: 2 }}
-							m={{ base: 1, md: 2 }}
-							ml={"2%"}
-						>
-							Dashboard
-						</Flex>
-					</Link>
-				</Flex>
-				<Flex>
-					{localStorage.getItem("token") == null && (
-						<Link to="/signup">
-							<Box
-								_hover={{
-									backgroundColor: "rgba(255, 255, 255, 0.1)",
-									borderRadius: "5px",
-								}}
-								borderRadius={"5px"}
-								p={{ base: 1, md: 2 }}
-								m={{ base: 0, md: 2 }}
-							>
-								Signup
-							</Box>
-						</Link>
-					)}
-					{localStorage.getItem("token") != null ? (
+			<Flex alignItems={"center"}>
+				<Link to="/home">
+					<Box
+						bg={isActive("/home") ? "rgba(255, 255, 255, 0.1)" : ""}
+						p={{ base: 1, md: 2 }}
+						m={{ base: 0, md: 2 }}
+						borderRadius={"5px"}
+						_hover={{
+							backgroundColor: "rgba(255, 255, 255, 0.1)",
+							borderRadius: "5px",
+						}}
+					>
+						Home
+					</Box>
+				</Link>
+				<Link to="/about">
+					<Box
+						bg={
+							isActive("/about") ? "rgba(255, 255, 255, 0.1)" : ""
+						}
+						_hover={{
+							backgroundColor: "rgba(255, 255, 255, 0.1)",
+							borderRadius: "5px",
+						}}
+						borderRadius={"5px"}
+						p={{ base: 1, md: 2 }}
+						m={{ base: 0, md: 2 }}
+					>
+						About
+					</Box>
+				</Link>
+				<Link to="/services">
+					<Box
+						bg={
+							isActive("/services")
+								? "rgba(255, 255, 255, 0.1)"
+								: ""
+						}
+						borderRadius={"5px"}
+						_hover={{
+							backgroundColor: "rgba(255, 255, 255, 0.1)",
+							borderRadius: "5px",
+						}}
+						p={{ base: 1, md: 2 }}
+						m={{ base: 0, md: 2 }}
+					>
+						Services
+					</Box>
+				</Link>
+				<Link to={"/dashboard"}>
+					<Flex
+						bg={
+							isActive("/dashboard")
+								? "rgba(255, 255, 255, 0.1)"
+								: ""
+						}
+						borderRadius={"5px"}
+						borderWidth={"2px"}
+						width={"100%"}
+						alignItems={"center"}
+						justifyContent={"center"}
+						borderColor={"white"}
+						_hover={{
+							backgroundColor: "rgba(255, 255, 255, 0.1)",
+							borderRadius: "5px",
+						}}
+						p={{ base: 1, md: 2 }}
+						m={{ base: 1, md: 2 }}
+						ml={"2%"}
+					>
+						Dashboard
+					</Flex>
+				</Link>
+			</Flex>
+			<Flex>
+				{localStorage.getItem("token") == null && (
+					<Link to="/signup">
 						<Box
 							_hover={{
 								backgroundColor: "rgba(255, 255, 255, 0.1)",
@@ -135,33 +111,44 @@ function NavbarMenu() {
 							borderRadius={"5px"}
 							p={{ base: 1, md: 2 }}
 							m={{ base: 0, md: 2 }}
-							onClick={() => {
-								// localStorage.removeItem("token");
-
-								logout().then(() => {
-									navigate("/home");
-								});
-							}}
-							cursor={"pointer"}
 						>
-							Logout
+							Signup
 						</Box>
-					) : (
-						<Link to="/login">
-							<Box
-								_hover={{
-									backgroundColor: "rgba(255, 255, 255, 0.1)",
-									borderRadius: "5px",
-								}}
-								borderRadius={"5px"}
-								p={{ base: 1, md: 2 }}
-								m={{ base: 0, md: 2 }}
-							>
-								Login
-							</Box>
-						</Link>
-					)}
-				</Flex>
+					</Link>
+				)}
+				{localStorage.getItem("token") != null ? (
+					<Box
+						_hover={{
+							backgroundColor: "rgba(255, 255, 255, 0.1)",
+							borderRadius: "5px",
+						}}
+						borderRadius={"5px"}
+						p={{ base: 1, md: 2 }}
+						m={{ base: 0, md: 2 }}
+						onClick={() => {
+							logout().then(() => {
+								navigate("/home");
+							});
+						}}
+						cursor={"pointer"}
+					>
+						Logout
+					</Box>
+				) : (
+					<Link to="/login">
+						<Box
+							_hover={{
+								backgroundColor: "rgba(255, 255, 255, 0.1)",
+								borderRadius: "5px",
+							}}
+							borderRadius={"5px"}
+							p={{ base: 1, md: 2 }}
+							m={{ base: 0, md: 2 }}
+						>
+							Login
+						</Box>
+					</Link>
+				)}
 			</Flex>
 		</Flex>
 	);
